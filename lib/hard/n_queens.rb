316985aa46785b465cfdf3515659aee74dd934ee
Yongmin Xia <yongmin.xia@rea-group.com>
2014-11-04 03:39:48 +1100

    Added solution to place queens on chessboard

--- lib/hard/n_queens.rb
+++ lib/hard/n_queens.rb
@@ -3,7 +3,7 @@ module Hard
   class NQueens
 
     def self.place(board_size)
-      []
+      place_recursive(board_size, board_size)
     end
 
     def self.display(queens)
@@ -12,6 +12,26 @@ module Hard
       }
       puts rows.join("\n")
       puts "\n"
+    end
+
+    private
+    def self.place_recursive(board_size, row)
+      return [[]] if row == 0
+      current_solutions = place_recursive(board_size, row - 1)
+      result = []
+      current_solutions.each do |queens|
+        (0...board_size).each do |col|
+          result.push([col] + queens) if is_safe?(col, queens)
+        end
+      end
+      result
+    end
+
+    def self.is_safe?(col, queens)
+      queens.each_with_index do |c, index|
+        return false if (col == c) || ((col - c).abs == (index + 1))
+      end
+      true
     end
   end
 end
