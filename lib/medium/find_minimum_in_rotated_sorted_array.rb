cce92c897f77ee52473532fb240fb91e6bbe03a2
Yongmin Xia <yongmin.xia@rea-group.com>
2014-11-03 20:56:26 +1100

    added solution to find min number in a rotated sorted array

--- lib/medium/find_minimum_in_rotated_sorted_array.rb
+++ lib/medium/find_minimum_in_rotated_sorted_array.rb
@@ -3,6 +3,12 @@ module Medium
   class FindMinimumInRotatedSortedArray
 
     def self.find_min(arr)
+      p = 0
+      q = arr.size - 1
+      while(arr[p] > arr[q])
+        p = p + 1
+      end
+      arr[p]
     end
 
   end
